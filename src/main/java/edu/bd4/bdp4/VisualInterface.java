c0586d7b867634bb50aff6c49b8ee75c4e13bf94
Alejandro Lana Mediavilla <alejandrolana1@gmail.com>
2024-04-17 21:05:44 +0200

    Push Con CHecks

--- src/main/java/edu/bd4/bdp4/VisualInterface.java
+++ src/main/java/edu/bd4/bdp4/VisualInterface.java
@@ -1,7 +1,10 @@
 package edu.bd4.bdp4;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,6 +16,12 @@ public class VisualInterface {
     private TextArea atributesTextArea;
     @FXML
     private Button createTableBtn;
+    @FXML
+    private Label errorLabel1;
+    @FXML
+    private Label errorLabel2;
+    @FXML
+    private Label errorLabel3;
 
     public VisualInterface() {
     }
@@ -22,6 +31,7 @@ public class VisualInterface {
     protected void onCreateTableBtn() {
         String tableNameString = tableName.getText();
         ArrayList <String> Tabla = generateAtributesArrayList();
+        checkAtributes(Tabla);
         //JDBC.createTable(tableNameString, Tabla);
     }
 
@@ -35,5 +45,42 @@ public class VisualInterface {
             atributes.add(line.trim()); // trim() para eliminar espacios en blanco adicionales al principio y al final de cada línea
         }
         return atributes;
+    }
+    private void checkAtributes(ArrayList<String> atributes) {
+        String[] validDataTypes = {"INT", "VARCHAR", "TEXT", "DATE", "TIMESTAMP", "FLOAT", "DOUBLE", "DECIMAL", "BOOLEAN"};
+        String[] validRestrictions = {"NOT_NULL", "UNIQUE", "PRIMARY_KEY", "FOREIGN_KEY", "CHECK", "DEFAULT", "INDEX", "AUTO_INCREMENT"};
+        int contador = 0;
+
+        for (String atribute : atributes) {
+            contador++;
+            String[] atributeData = atribute.split(" ");
+
+            // Check attribute name
+            if (!atributeData[0].matches("[a-zA-Z]+")) {
+                errorLabel1.setText("Invalid attribute name: " + atributeData[0] + " from " +contador + "º Atribute" );
+                changeOpacityOnAndOff(errorLabel1);
+            }
+
+            // Check data type
+            if (!Arrays.asList(validDataTypes).contains(atributeData[1].toUpperCase())) {
+                errorLabel2.setText("Invalid data type: " + atributeData[1] + "from " +contador + " Atribute" );
+                changeOpacityOnAndOff(errorLabel2);
+            }
+
+            // Check restrictions
+            for (int i = 2; i < atributeData.length; i++) {
+                if (!Arrays.asList(validRestrictions).contains(atributeData[i].toUpperCase())) {
+                    errorLabel3.setText("Invalid restriction: " + atributeData[i] + "from " +contador + " Atribute" );
+                    changeOpacityOnAndOff(errorLabel3);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void changeOpacityOnAndOff(Label label) {
+        label.setOpacity(1);
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(7), event -> label.setOpacity(0)));
+        timeline.play();
     }
 }
